4d5415c76f335f36c2b319a4181e6bfef033d0f8
ElenaRalucaBarbacariu03 <161924121+ElenaRalucaBarbacariu03@users.noreply.github.com>
2024-07-27 12:58:11 +0300

    Add files via upload

---
+++ BookingApp/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtcheck = new System.Windows.Forms.TextBox();
+            this.btnBook = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtnumberUSER = new System.Windows.Forms.TextBox();
+            this.txtpriceDB = new System.Windows.Forms.TextBox();
+            this.cboRooms = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtcheck
+            // 
+            this.txtcheck.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtcheck.Font = new System.Drawing.Font("Microsoft New Tai Lue", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcheck.Location = new System.Drawing.Point(35, 897);
+            this.txtcheck.Multiline = true;
+            this.txtcheck.Name = "txtcheck";
+            this.txtcheck.ReadOnly = true;
+            this.txtcheck.Size = new System.Drawing.Size(946, 62);
+            this.txtcheck.TabIndex = 39;
+            this.txtcheck.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnBook
+            // 
+            this.btnBook.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnBook.Font = new System.Drawing.Font("Modern No. 20", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBook.Location = new System.Drawing.Point(420, 823);
+            this.btnBook.Name = "btnBook";
+            this.btnBook.Size = new System.Drawing.Size(167, 43);
+            this.btnBook.TabIndex = 33;
+            this.btnBook.Text = "Book now!";
+            this.btnBook.UseVisualStyleBackColor = false;
+            this.btnBook.Click += new System.EventHandler(this.btnBook_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtPhone);
+            this.groupBox2.Controls.Add(this.txtEmail);
+            this.groupBox2.Controls.Add(this.txtName);
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(247, 558);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(491, 247);
+            this.groupBox2.TabIndex = 35;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Fill and book your room now";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPhone.Location = new System.Drawing.Point(189, 184);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(267, 29);
+            this.txtPhone.TabIndex = 23;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(163, 123);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(293, 29);
+            this.txtEmail.TabIndex = 24;
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(163, 61);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(293, 29);
+            this.txtName.TabIndex = 25;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(12, 185);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(159, 25);
+            this.label11.TabIndex = 23;
+            this.label11.Text = "Phone number";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 124);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 25);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Email";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 62);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(116, 25);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Full name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 25);
+            this.label5.TabIndex = 10;
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Enabled = false;
+            this.txtAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAmount.Location = new System.Drawing.Point(220, 495);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(185, 27);
+            this.txtAmount.TabIndex = 29;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker2.Location = new System.Drawing.Point(555, 331);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(346, 27);
+            this.dateTimePicker2.TabIndex = 38;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(95, 331);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(345, 27);
+            this.dateTimePicker1.TabIndex = 37;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(42, 494);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(160, 25);
+            this.label8.TabIndex = 30;
+            this.label8.Text = "Amount to pay";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(216, 294);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 22);
+            this.label4.TabIndex = 36;
+            this.label4.Text = "Check-In";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(679, 294);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 22);
+            this.label3.TabIndex = 34;
+            this.label3.Text = "Check-Out";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(42, 223);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(162, 25);
+            this.label10.TabIndex = 32;
+            this.label10.Text = "Price per room";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(42, 171);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(185, 25);
+            this.label9.TabIndex = 31;
+            this.label9.Text = "Number of rooms";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnCalculate.Font = new System.Drawing.Font("Modern No. 20", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(366, 413);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(247, 43);
+            this.btnCalculate.TabIndex = 28;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtnumberUSER
+            // 
+            this.txtnumberUSER.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnumberUSER.Location = new System.Drawing.Point(274, 170);
+            this.txtnumberUSER.Name = "txtnumberUSER";
+            this.txtnumberUSER.Size = new System.Drawing.Size(114, 29);
+            this.txtnumberUSER.TabIndex = 27;
+            // 
+            // txtpriceDB
+            // 
+            this.txtpriceDB.Enabled = false;
+            this.txtpriceDB.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpriceDB.Location = new System.Drawing.Point(260, 219);
+            this.txtpriceDB.Name = "txtpriceDB";
+            this.txtpriceDB.Size = new System.Drawing.Size(107, 29);
+            this.txtpriceDB.TabIndex = 26;
+            // 
+            // cboRooms
+            // 
+            this.cboRooms.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.cboRooms.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboRooms.FormattingEnabled = true;
+            this.cboRooms.Items.AddRange(new object[] {
+            "Standard Room (2 people)",
+            "Family Room (4 people)",
+            "Apartament (from 8 to 10 people)"});
+            this.cboRooms.Location = new System.Drawing.Point(203, 122);
+            this.cboRooms.Name = "cboRooms";
+            this.cboRooms.Size = new System.Drawing.Size(253, 33);
+            this.cboRooms.TabIndex = 25;
+            this.cboRooms.SelectedIndexChanged += new System.EventHandler(this.cboRooms_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(42, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 25);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Room Type";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(348, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(326, 34);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Mini Booking System";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1016, 1019);
+            this.Controls.Add(this.txtcheck);
+            this.Controls.Add(this.btnBook);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtnumberUSER);
+            this.Controls.Add(this.txtpriceDB);
+            this.Controls.Add(this.cboRooms);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtcheck;
+        private System.Windows.Forms.Button btnBook;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtnumberUSER;
+        private System.Windows.Forms.TextBox txtpriceDB;
+        private System.Windows.Forms.ComboBox cboRooms;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BookingApp/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OleDbConnection conexiune;
+        OleDbCommand comanda;
+        OleDbDataReader reader;
+        DataTable dt;
+        DataSet ds;
+
+        private void cboRooms_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            conexiune = new OleDbConnection();
+            conexiune.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\barba\Desktop\C#-extra-apps\BookingApp\BookingSystem.accdb;Persist Security Info=False";
+            conexiune.Open();
+
+            comanda = new OleDbCommand();
+            comanda.Connection = conexiune;
+            comanda.CommandText = "SELECT Price FROM Rooms WHERE RoomType = ?";
+            comanda.Parameters.Clear();
+            comanda.Parameters.AddWithValue("RoomType", cboRooms.SelectedItem.ToString());
+
+            reader = comanda.ExecuteReader();
+
+            dt = new DataTable("Rooms");
+            dt.Load(reader);
+
+            ds = new DataSet();
+            ds.Tables.Add(dt);
+
+            txtpriceDB.Text = ds.Tables["Rooms"].Rows[0].ItemArray[0].ToString();
+
+            conexiune.Close();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            DateTime date1 = dateTimePicker1.Value;
+            DateTime date2 = dateTimePicker2.Value;
+            TimeSpan time = date2 - date1;
+            int totalDays = (int)time.TotalDays;
+            //MessageBox.Show(totalDays.ToString());
+            txtAmount.Text = (int.Parse(txtnumberUSER.Text) * int.Parse(txtpriceDB.Text) * totalDays).ToString();
+        }
+
+        private void btnBook_Click(object sender, EventArgs e)
+        {
+            txtcheck.Text = "Thank you for your booking, dear " + txtName.Text + "! You booked " + txtnumberUSER.Text + " " + cboRooms.SelectedItem.ToString() + " in our hotel from " + dateTimePicker1.Value.ToShortDateString() + " to " + dateTimePicker2.Value.ToShortDateString() + ".";
+        }
+    }
+}
